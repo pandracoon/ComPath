e8d5ad7a9231e28524a5f64447185b6ecd8a501b
hism1125 <55496297+hism1125@users.noreply.github.com>
2019-10-01 22:38:42 +0900

    login

--- app/src/main/java/com/example/compath/LoginActivity.java
+++ app/src/main/java/com/example/compath/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.compath;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +16,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+    }
+
+    public void Login(View v){
+        EditText id = (EditText)findViewById(R.id.id_input);
+        EditText pw = (EditText)findViewById(R.id.password_input);
+
+        final String ID = id.getText().toString();
+        final String PW = pw.getText().toString();
+
+        if(ID == "compath" && PW == "123456"){
+            Intent intent = new Intent(this, LoadingActivity.class);
+            startActivity(intent);
+        }
     }
 }
